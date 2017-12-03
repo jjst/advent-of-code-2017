46ddeaaebc1d145c93cd81721fb083dc543e259b
Jeremie Jost <jeremiejost@gmail.com>
2017-12-03 18:45:50 +0000

    Day 1 - part 2

--- day1/src/lib.rs
+++ day1/src/lib.rs
@@ -1,10 +1,20 @@
 
-pub fn solution(input: &str) -> u32 {
-    let digits: Vec<u32> = input.chars().map(|c| c.to_digit(10).unwrap()).collect();
-    println!("{:?}", digits);
+pub fn digits(input: &str) -> Vec<u32> {
+    input.chars().map(|c| c.to_digit(10).unwrap()).collect()
+}
+
+pub fn captcha1(digits: &[u32]) -> u32 {
+    identical_digit_sum(digits, 1)
+}
+
+pub fn captcha2(digits: &[u32]) -> u32 {
+    identical_digit_sum(digits, digits.len() / 2)
+}
+
+pub fn identical_digit_sum(digits: &[u32], skipahead: usize) -> u32 {
     digits
         .iter()
-        .zip(digits.iter().cycle().skip(1))
+        .zip(digits.iter().cycle().skip(skipahead))
         .filter_map(|(n1,n2)| if n1 == n2 { Some(n1) } else { None })
         .sum()
 }
